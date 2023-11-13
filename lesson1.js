2e40c22bb858dc2134bbee16cc5b067c8acbbc24
KimTien <kimtien15071997@gmail.com>
2023-11-13 10:10:59 +0700

    add lesson1

---
+++ lesson1.js
@@ -0,0 +1,55 @@
+//console.log("tui ne")
+// {let a=999;
+// {
+//     console.log (a);
+// }
+// console.log (a)
+// }
+// const user = {
+//     name: 'tien',
+//     phone: '12'
+// }; 
+
+// user['name'] = 'thanh'
+// console.log(user)
+
+// let a = 1
+// {
+//     const a = 1
+// }
+// let a = 567890;
+// let c = 1234;
+// let b = a-c;
+// console.log(a-c);
+
+// let a=3;
+// console.log(a);
+// console.log(a);
+
+// dac biet khac laf ham so mu
+// let a = 3;
+// let b =5;
+// a = a+b; => a += b; ( or a **= b)
+// console.log(a);
+
+// let a=12;
+// let b = 12;
+// console.log (a >=b)
+
+// => Neu muon thu console.log =(a=b);
+// => k duoc 
+// se phai if(a=b){
+
+// }
+//console.log("5"===5);
+
+
+// let a = "undefineed";
+// console.log(a);
+// if {
+//     a== undefined
+//     }
+// }
+// / and , or
+let a =5;
+console.log (a > 3 && b < 6);  
