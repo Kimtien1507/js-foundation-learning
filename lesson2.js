3e663a85945383c888a354e4c516b27865afca33
KimTien <kimtien15071997@gmail.com>
2023-11-15 09:38:45 +0700

    lesson 2

---
+++ lesson2.js
@@ -0,0 +1,91 @@
+// Gan toan tu 3 ngoi
+// let isMale = false & true;
+// let a = !isMale ? 18: 20;
+// console.log(a);
+//
+//toan tu gan / 60, null, undefine, va k gan gi
+// neu gan null or undefine thi se lay thang phia sau (??)
+// a= null;
+// b=a??0;
+// console.log(b);
+
+// let a = 0.2;
+// let b = null;
+// console.log(a+b);
+
+// console.log("20"/"10");
+// let b = 10;
+// console.log(b/"abc");
+// // khong bao loi
+// console.log(-b/0);
+// console.log(typeof null);
+
+// khai bao bang new (van duoc, nhung kh nen su dung)
+// let a = new Number(10);
+// let b = new Number(10);
+// console.log(a==b);
+
+// 
+// console.log(Number.isNaN("undefine"));
+// console.log(Number.MAX_SAFE_INTEGER);
+// 
+//console.log(Math.round(a));
+//console.log((12.4456789098765567890).toFixed(46)); // ???????
+//
+//String Ex: Hello Pamyeuoi
+// console.log("hello \"Pamyeuoi\"");
+// console.log('name');
+
+//console.log("name"== "12345"); => false
+
+// console.log("abc4657".substring(1,5));
+
+// console.log("abc123".toLocaleLowerCase());
+// console.log("12345".toLocaleLowerCase());
+//
+//console.log("1234567".replace("9"&"3","1", "FFFF")); 
+
+// try more (scope khac nhau)
+// {
+//     let a=2;
+//     {
+//         let = 5;
+//         for(let a=0; a<10; a++){
+//             let a=0.2;
+//             console.log(a);
+//         }
+//     }
+// }
+// 
+/////////// console.log("pamyeuoi pamyeuoi pamyeuoi1 ".replaceAll(/p/g,"miu"));
+/////// console.log("       123".trim());
+
+// Object ( cach to chuc doi tuong)
+// let a = {
+//     name:"Pam",
+//     "age": 20,
+//     "male": "male?",
+//     1: "sdfghj"
+// }
+// console.log(a.male);
+// // neu key la so 
+// //console.log(a["1"]);
+// //
+// console.log(Object.keys(a));
+const obj = {a:4, b:0}
+obj.a = 2;
+console.log(obj);
+
+console.log(JSON.stringify(obj));
+
+
+
+
+
+
+
+
+
+
+
+
